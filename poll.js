8617abf075fa7625bf7136bb0660511a98c0cad1
Andrew Wong <andrew.wong@creditkarma.com>
2013-09-06 17:11:28 -0700

    Using underscore to check if an element is empty

--- poll.js
+++ poll.js
@@ -1,4 +1,5 @@
 var request = require('request'),
+    _       = require('underscore'),
     cheerio = require('cheerio');
 
 var poll_craigslist = function(callback) {
@@ -9,25 +10,35 @@ var poll_craigslist = function(callback) {
 
         $ = cheerio.load(body);
 
-        $('p.row').each(function(idx, elem) {
-            var $this     = $(this),
-                post_id   = $this.attr('data-pid'),
-                latitude  = $this.attr('data-latitude'),
-                longitude = $this.attr('data-longitude'),
-                anchor    = $this.children('a').attr('href'),
-                title     = $this.children('.pl').children('a').text();
-                
-            console.log(title);
-
-            if (latitude === undefined && longitude === undefined) {
-                // Need to figure out what to do later if we dont have the longitude and latitude
-                // var location = $this.children('.l2').children('.pnr').children('small').text();
-                return;
-            }
-
-
-        });
+        var data = $('p.row')
+            .map(body_extractor)
+            .filter(function(elem, idx) {
+                return !_.isEmpty(elem);
+            });
     });
+};
+
+var body_extractor = function(idx, elem) {
+    var $this     = $(this),
+        post_id   = $this.attr('data-pid'),
+        latitude  = $this.attr('data-latitude'),
+        longitude = $this.attr('data-longitude'),
+        anchor    = $this.children('a').attr('href'),
+        title     = $this.children('.pl').children('a').text();
+
+    if (latitude === undefined && longitude === undefined) {
+        // Need to figure out what to do later if we dont have the longitude and latitude
+        // var location = $this.children('.l2').children('.pnr').children('small').text();
+        return {};
+    }
+
+    return {
+        "post_id": post_id,
+        "longitude": longitude,
+        "latitude": latitude,
+        "anchor": anchor,
+        "title": title
+    };
 };
 
 poll_craigslist();
