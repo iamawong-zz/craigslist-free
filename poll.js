d55d1e0b6175fb37adaf5e37652699105cc1e5c2
Andrew Wong <andrew.wong@creditkarma.com>
2013-09-06 17:29:58 -0700

    Storing things into redis database

--- poll.js
+++ poll.js
@@ -1,8 +1,37 @@
 var request = require('request'),
     _       = require('underscore'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    redis   = require('redis'),
+    db      = redis.createClient();
 
 var poll_craigslist = function(callback) {
+    var body_extractor = function(idx, elem) {
+        var $this     = $(this),
+            post_id   = $this.attr('data-pid'),
+            latitude  = $this.attr('data-latitude'),
+            longitude = $this.attr('data-longitude'),
+            anchor    = $this.children('a').attr('href'),
+            title     = $this.children('.pl').children('a').text();
+
+        if (latitude === undefined && longitude === undefined) {
+            // Need to figure out what to do later if we dont have the longitude and latitude
+            // var location = $this.children('.l2').children('.pnr').children('small').text();
+            return {};
+        }
+
+        return {
+            "post_id": post_id,
+            "longitude": longitude,
+            "latitude": latitude,
+            "anchor": anchor,
+            "title": title
+        };
+    };
+
+    var not_empty = function(elem, idx) {
+        return !_.isEmpty(elem);
+    };
+
     request('http://sfbay.craigslist.org/sfc/zip/', function(error, response, body) {
         if (error || response.statusCode != 200) {
             callback('', '', '');
@@ -10,35 +39,9 @@ var poll_craigslist = function(callback) {
 
         $ = cheerio.load(body);
 
-        var data = $('p.row')
-            .map(body_extractor)
-            .filter(function(elem, idx) {
-                return !_.isEmpty(elem);
-            });
+        var data = $('p.row').map(body_extractor).filter(not_empty);
+        db.set('recent', data);
     });
-};
-
-var body_extractor = function(idx, elem) {
-    var $this     = $(this),
-        post_id   = $this.attr('data-pid'),
-        latitude  = $this.attr('data-latitude'),
-        longitude = $this.attr('data-longitude'),
-        anchor    = $this.children('a').attr('href'),
-        title     = $this.children('.pl').children('a').text();
-
-    if (latitude === undefined && longitude === undefined) {
-        // Need to figure out what to do later if we dont have the longitude and latitude
-        // var location = $this.children('.l2').children('.pnr').children('small').text();
-        return {};
-    }
-
-    return {
-        "post_id": post_id,
-        "longitude": longitude,
-        "latitude": latitude,
-        "anchor": anchor,
-        "title": title
-    };
 };
 
 poll_craigslist();
