04b00f264785d77b9c591d6cceee96b0ddb15359
Andrew Wong <iamawong@outlook.com>
2013-09-08 13:38:39 -0700

    setting up crappy response to output json data

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var express = require('express');
+var express = require('express'),
+	poll    = require('./poll'),
+	redis   = require('redis'),
+	db      = redis.createClient();
 
 var app = express();
 
@@ -13,5 +16,27 @@ if (process.env.NODE_ENV == 'production') {
 app.use(express.static(__dirname + '/public', {maxAge: maxAge}));
 app.use(express.compress());
 // app.use(express.logger());
+
+app.get('/', function(request, response) {
+	var get_craigslist = function(callback) {
+		db.get('free', function(err, result) {
+			if (err) {
+				callback({});
+				return;
+			}
+
+			if (result) {
+				callback(result);
+				return;
+			}			
+
+			poll.craigslist(callback);
+		});
+	};
+
+	get_craigslist(function(data) {
+		response.send(data);
+	});
+});
 
 app.listen(3000);
